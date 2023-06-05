a38c6337d5d202214082aeec1364cca70a4621e8
OhBeomho <tyler0322@naver.com>
2023-06-05 19:40:42 +0900

    server

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,4 @@
-const SERVER_URL = import.meta.env.DEV ? "http://localhost:5000" : "https://ops.onrender.com";
+const SERVER_URL = import.meta.env.DEV ? "http://localhost:5000" : "https://ops-wswd.onrender.com";
 
 async function check(res: Response) {
   const data = await res.json();
